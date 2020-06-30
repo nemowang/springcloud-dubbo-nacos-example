a755e5c4ced8f09555c459e30aa33c8ffe1dd4b2
Nemo <nemowang77@163.com>
2020-06-30 15:13:45 +0800

    Initial commit

---
+++ spring-cloud-dubbo-consumer/src/main/java/com/nemo/consumer/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.nemo.consumer.controller;
+
+import com.nemo.api.service.TestService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Reference(check = false)
+    private TestService testService;
+
+    @GetMapping("sayHello")
+    public String sayHello(String name) {
+        return testService.sayHello(name);
+    }
+}
