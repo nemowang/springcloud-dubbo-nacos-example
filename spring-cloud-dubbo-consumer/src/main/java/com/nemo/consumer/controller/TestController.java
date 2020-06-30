b97e8224dc318685438207fbe5166bc510fb5687
Nemo <nemowang77@163.com>
2020-06-30 16:27:12 +0800

    修改pom文件

--- spring-cloud-dubbo-consumer/src/main/java/com/nemo/consumer/controller/TestController.java
+++ spring-cloud-dubbo-consumer/src/main/java/com/nemo/consumer/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @Reference(check = false)
+    @Reference
     private TestService testService;
 
     @GetMapping("sayHello")
